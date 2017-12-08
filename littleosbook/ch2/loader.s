1468fa86116703ab0db42b187416e8abdf4ed4ac
Akshat Harit <akshat.harit@gmail.com>
2017-12-07 17:20:19 -0800

    Added comments for sanity

--- littleosbook/ch2/loader.s
+++ littleosbook/ch2/loader.s
@@ -1,17 +1,17 @@
-global loader
+global loader				; Symbol for elf
 
-MAGIC_NUMBER 	equ 	0x1BADB002
-FLAGS 		equ	0x0
-CHECKSUM	equ	-MAGIC_NUMBER
+MAGIC_NUMBER 	equ 	0x1BADB002	; Number for grub multiboot specification
+FLAGS 		equ	0x0		; multiboot flags
+CHECKSUM	equ	-MAGIC_NUMBER	; magic_num+checksum+flags must be 0x0
 
 section .text:
-align 4
-	dd MAGIC_NUMBER
-	dd FLAGS
+align 4					; Must be 4 byte aligned
+	dd MAGIC_NUMBER			
+	dd FLAGS			
 	dd CHECKSUM
 
 loader:
-	mov eax, 0xCAFEBABE
+	mov eax, 0xCAFEBABE		
 .loop:
-	jmp .loop
+	jmp .loop			; loop forever
 
